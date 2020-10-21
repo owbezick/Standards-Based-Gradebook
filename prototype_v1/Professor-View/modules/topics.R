e0d476405a69966f35f8a9fc6cee8a83c26d7f72
Owen Bezick <owbezick@davidson.edu>
2020-10-21 12:46:03 -0400

    #41 topics.R

--- prototype_v1/Professor-View/modules/topics.R
+++ prototype_v1/Professor-View/modules/topics.R
@@ -1,8 +1,8 @@
 topics_UI <- function(id){
   box(width = 12, title = "Review Table", status = "primary"
       
-            , rHandsontableOutput(NS(id, "topicTable"))
-            , footer = "Right click to add a row to the table."
+      , rHandsontableOutput(NS(id, "topicTable"))
+      , footer = "Right click to add a row to the table."
       
       , actionBttn(NS(id, "save"), "Save", style = "material-flat", block = T)
       
@@ -39,14 +39,16 @@ topics_server <- function(id, r){
       `%notin%` <- Negate(`%in%`)
       
       # Save review_grades ----
-      df_review_to_topic <- r$df_review_grades
+      df_review_grades <- r$df_review_grades
+      
+      
       df_student <- r$df_student %>%
         select(student_id)
       
       data_from_hot <- df_hot %>%
         pivot_longer(cols = c(4:ncol(df_hot))) %>%
         na.omit() 
-        
+      
       review_topic_id_hot <- data_from_hot %>%
         filter(value == "TRUE")
       
@@ -61,12 +63,19 @@ topics_server <- function(id, r){
       # replicate student_id the for as many topics being added
       student_id <- do.call("rbind", replicate(nrow(review_topic_id_hot), df_student, simplify = FALSE)) %>%
         arrange(student_id)
-        
+      
       df_new_review_data <- new_review_topic_rep %>%
         mutate(student_id = student_id$student_id
-               , grade = rep("NA", nrow(new_review_topic_rep))) 
+               , grade = rep("NA", nrow(new_review_topic_rep))
+               , id = paste(review_id, topic_id, student_id)) 
+      df_review_grades <- df_review_grades %>%
+        mutate(id = paste(review_id, topic_id, student_id))
       
-      temp <- full_join(df_review_to_topic, df_new_review_data) %>%
+      df_new_review_data <- subset(df_new_review_data, id != df_review_grades$id) %>%
+        select(-c(id))
+      df_review_grades <- df_review_grades %>%
+        select(-c(id))
+      temp <- rbind(df_review_grades, df_new_review_data) %>%
         arrange(review_id, topic_id)
       
       
@@ -78,84 +87,86 @@ topics_server <- function(id, r){
         , sheet = "review_grades"
       )
       
+      
+      
       showNotification("Saved to remote.")
     })
-      # data_from_hot <- df %>%
-      #   pivot_longer(cols = c(4:ncol(df))) %>%
-      #   na.omit()
-      # 
-      # data_from_hot <- data_from_hot %>%
-      #   mutate(topic_id = str_split_fixed(data_from_hot$name, " ", 2)[,2]) %>%
-      #   select(review_id = `Review ID`, topic_id)
-      # 
-      # ls_distinct_new_review_id <- data_from_hot %>%
-      #   select(review_id) %>%
-      #   distinct() %>%
-      #   pull()
-      # 
-      # 
-      # ls_distinct_old_review_id <-df_review_to_topic %>%
-      #   select(review_id) %>%
-      #   distinct() %>%
-      #   pull()
-      # 
-      # # Check for any deletions, and remove
-      # temp <- subset(df_review_to_topic, review_id %in% ls_distinct_new_review_id)
-      # 
-      # # Check if topics in reviews have changed 
-      # prev_review_topic_id <- temp %>%
-      #   group_by(review_id, topic_id) %>%
-      #   distinct(topic_id) %>%
-      #   ungroup() %>%
-      #   group_by(review_id)
-      # 
-      # new_review_topic_id <- data_from_hot %>%
-      #   group_by(review_id)
-      # 
-      # new_review_topic_id <- subset(new_review_topic_id, topic_id %notin% prev_review_topic_id$topic_id)
-      # 
-      # if (nrow(new_review__topic_id) > 0){
-      #   # Replicate for each student
-      #   new_review__topic_rep <- do.call("rbind", replicate(nrow(df_student_id), new_review_topic_id, simplify = FALSE))
-      # }
-      # 
-      # 
-      # # replicate student_id the for as many topics being added
-      # student_id <- do.call("rbind", replicate(nrow(new_review_topic_id), df_student_id, simplify = FALSE)) %>%
-      #   arrange(student_id)
-      # 
-      # df_new_review_data <- new_review_rep %>%
-      #   mutate(student_id = student_id$student_id
-      #          , grade = rep("NA", nrow(new_review_rep)))
-      # 
-      # 
-      # temp <- rbind(temp, df_new_review_data) %>%
-      #   arrange(review_id, topic_id)
-      #                            
-      # # Check for any new reviews
-      # new_review_data <- subset(data_from_hot, review_id %notin% ls_distinct_old_review_id)
-      # if(nrow(new_review_data) > 0){
-      #   # Add in any new reviews with student grades as NA
-      #   df_student_id <- r$df_student %>%
-      #     select(student_id) 
-      #   
-      #   # replicate review_id and topic_id for as many students that are in the class
-      #   new_review_rep <- do.call("rbind", replicate(nrow(df_student_id), new_review_data, simplify = FALSE))
-      #   
-      #   # replicate student_id the for as many topics being added
-      #   student_id <- do.call("rbind", replicate(nrow(new_review_data), df_student_id, simplify = FALSE)) %>%
-      #     arrange(student_id)
-      #   
-      #   df_new_review_data <- new_review_rep %>%
-      #     mutate(student_id = student_id$student_id
-      #            , grade = rep("NA", nrow(new_review_rep)))
-      #   
-      #   
-      #   temp <- rbind(temp, df_new_review_data) %>%
-      #     arrange(review_id, topic_id)
-      # }
-      
-   
+    # data_from_hot <- df %>%
+    #   pivot_longer(cols = c(4:ncol(df))) %>%
+    #   na.omit()
+    # 
+    # data_from_hot <- data_from_hot %>%
+    #   mutate(topic_id = str_split_fixed(data_from_hot$name, " ", 2)[,2]) %>%
+    #   select(review_id = `Review ID`, topic_id)
+    # 
+    # ls_distinct_new_review_id <- data_from_hot %>%
+    #   select(review_id) %>%
+    #   distinct() %>%
+    #   pull()
+    # 
+    # 
+    # ls_distinct_old_review_id <-df_review_to_topic %>%
+    #   select(review_id) %>%
+    #   distinct() %>%
+    #   pull()
+    # 
+    # # Check for any deletions, and remove
+    # temp <- subset(df_review_to_topic, review_id %in% ls_distinct_new_review_id)
+    # 
+    # # Check if topics in reviews have changed 
+    # prev_review_topic_id <- temp %>%
+    #   group_by(review_id, topic_id) %>%
+    #   distinct(topic_id) %>%
+    #   ungroup() %>%
+    #   group_by(review_id)
+    # 
+    # new_review_topic_id <- data_from_hot %>%
+    #   group_by(review_id)
+    # 
+    # new_review_topic_id <- subset(new_review_topic_id, topic_id %notin% prev_review_topic_id$topic_id)
+    # 
+    # if (nrow(new_review__topic_id) > 0){
+    #   # Replicate for each student
+    #   new_review__topic_rep <- do.call("rbind", replicate(nrow(df_student_id), new_review_topic_id, simplify = FALSE))
+    # }
+    # 
+    # 
+    # # replicate student_id the for as many topics being added
+    # student_id <- do.call("rbind", replicate(nrow(new_review_topic_id), df_student_id, simplify = FALSE)) %>%
+    #   arrange(student_id)
+    # 
+    # df_new_review_data <- new_review_rep %>%
+    #   mutate(student_id = student_id$student_id
+    #          , grade = rep("NA", nrow(new_review_rep)))
+    # 
+    # 
+    # temp <- rbind(temp, df_new_review_data) %>%
+    #   arrange(review_id, topic_id)
+    #                            
+    # # Check for any new reviews
+    # new_review_data <- subset(data_from_hot, review_id %notin% ls_distinct_old_review_id)
+    # if(nrow(new_review_data) > 0){
+    #   # Add in any new reviews with student grades as NA
+    #   df_student_id <- r$df_student %>%
+    #     select(student_id) 
+    #   
+    #   # replicate review_id and topic_id for as many students that are in the class
+    #   new_review_rep <- do.call("rbind", replicate(nrow(df_student_id), new_review_data, simplify = FALSE))
+    #   
+    #   # replicate student_id the for as many topics being added
+    #   student_id <- do.call("rbind", replicate(nrow(new_review_data), df_student_id, simplify = FALSE)) %>%
+    #     arrange(student_id)
+    #   
+    #   df_new_review_data <- new_review_rep %>%
+    #     mutate(student_id = student_id$student_id
+    #            , grade = rep("NA", nrow(new_review_rep)))
+    #   
+    #   
+    #   temp <- rbind(temp, df_new_review_data) %>%
+    #     arrange(review_id, topic_id)
+    # }
+    
+    
     
   })
 }
